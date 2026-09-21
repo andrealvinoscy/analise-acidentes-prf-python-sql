216e092b74d603d41b10e1867f29f0bdc770167d
André Alvino <andrealvinoscy@gmail.com>
2026-09-21 10:43:43 -0300

    Update 01inspeção_de_dados.py
    inspeção 01 dados

--- python/01inspeção_de_dados.py
+++ python/01inspeção_de_dados.py
@@ -1,1 +1,33 @@
+import pandas as pd
 
+df = pd.read_csv(r"C:\Users\andre\OneDrive\Desktop\Projeto DATA\banco de dados\datatran2023.csv", sep = ';', encoding = 'latin1')
+
+print('Primeiras Linhas')
+print(df.head())
+
+---
+
+print('Dimensão da base')
+print(df.shape)
+
+---
+
+print('Colunas')
+print(df.columns.tolist())
+
+---
+
+print('Tipos de dados')
+print(df.dtypes)
+
+---
+
+print('Valores nulos')
+print(df.isnull().sum())
+
+---
+
+print('Informações gerais')
+df.info()
+
+#Inspeção básica - 01
